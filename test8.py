8584f30b2af584678a58ce76b1cbf40e2cdfb934
FrankBlood <1062947208@qq.com>
2015-12-09 18:15:45 +0800

    jilei

--- test8.py
+++ test8.py
@@ -1,8 +1,8 @@
 def printMax(x,y):
 	'''Prints the maximum of two numbers.
 	The two values must be integers'''
-	#x=int(x)
-	#y=int(y)
+	x=int(x)
+	y=int(y)
 	if x > y:
 		print(x,'is maximum')
 	else:
